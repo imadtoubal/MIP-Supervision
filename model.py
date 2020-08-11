843c63e1bfa7cd68cb0bea7b71d4cfdd48b023e3
Imad Toubal <imad.toubal@gmail.com>
2020-08-11 11:25:08 -0500

    updated model

--- model.py
+++ model.py
@@ -13,18 +13,19 @@ import torch.nn.functional as F
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+    def __init__(self, in_channels, out_channels, mid_channels=None, bn=False):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True)
-        )
+        self.double_conv = nn.Sequential()
+
+        self.double_conv.add_module('conv1', nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1))
+        if bn: self.double_conv.add_module('bn1', nn.BatchNorm3d(mid_channels))
+        self.double_conv.add_module('relu1', nn.ReLU(inplace=True))
+        self.double_conv.add_module('conv2', nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1))
+        if bn: self.double_conv.add_module('bn2', nn.BatchNorm3d(out_channels))
+        self.double_conv.add_module('relu2', nn.ReLU(inplace=True))
+    
 
     def forward(self, x):
         return self.double_conv(x)
@@ -33,11 +34,11 @@ class DoubleConv(nn.Module):
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, bn=False):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool3d(2),
-            DoubleConv(in_channels, out_channels)
+            DoubleConv(in_channels, out_channels, bn=bn)
         )
 
     def forward(self, x):
@@ -47,18 +48,18 @@ class Down(nn.Module):
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels, bilinear=True, bn=False):
         super().__init__()
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(
                 scale_factor=2, mode='trilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2, bn=bn)
         else:
             self.up = nn.ConvTranspose3d(
                 in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+            self.conv = DoubleConv(in_channels, out_channels, bn=bn)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -88,17 +89,17 @@ class OutConv(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True):
+    def __init__(self, n_channels, n_classes, bilinear=True, bn=False):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.up2 = Up(256 + 128, 128, bilinear)
-        self.up1 = Up(128 + 64, 64, bilinear)
+        self.inc = DoubleConv(n_channels, 64, bn=bn)
+        self.down1 = Down(64, 128, bn=bn)
+        self.down2 = Down(128, 256, bn=bn)
+        self.up2 = Up(256 + 128, 128, bilinear, bn=bn)
+        self.up1 = Up(128 + 64, 64, bilinear, bn=bn)
         self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
